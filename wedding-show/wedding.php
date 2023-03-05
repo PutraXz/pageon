ae1ef6d89be8525f8273ee1b429dbb60982a2834
putra <ytprem200100@gmail.com>
2023-03-05 18:42:11 +0700

    set slug

--- wedding-show/wedding.php
+++ wedding-show/wedding.php
@@ -1,5 +1,17 @@
-<?php
-    $name = $_SERVER['REQUEST_URI'];
-    echo $name;
-    // $test = array("", "", "", "");
-    // $test = $name;
+<?php 
+    include '../koneksi.php';
+    $name = explode('/',  $_SERVER['REQUEST_URI']);
+    $test = array("", "", "", "");
+    $test = $name;
+    if($test[2] == 'wedding'){
+        $get = $conn->query("select * from weddings")->fetch_array();
+        if($test[3] == $get['name_url']){
+            echo 'berhasil';
+        }else{
+            echo "
+                <script>
+                window.location.href='/slug';
+                </script>
+            ";
+        }
+    }
