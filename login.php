f5318df2bee42e69587f17d5ce00545d76b99fce
putra <ytprem200100@gmail.com>
2023-03-12 12:35:41 +0700

    change logic

--- login.php
+++ login.php
@@ -38,24 +38,32 @@
       $_SESSION['user_id'] = $data['user_id'];
       $_SESSION['username'] = $data['username'];
       $_SESSION['level'] = $data['level'];
-      $ip = $conn->query("SELECT COUNT(*) as count FROM computers where user_id='$_SESSION[user_id]' ")->fetch_array();
-      if($ip['count'] < 2){
-        $get_ip = $_SERVER['REMOTE_ADDR'];
-        $query = $conn->query("INSERT INTO computers set ip_address='$get_ip',user_id='$_SESSION[user_id]'");
+
+      // Jika user belum login sebelumnya, simpan IP ke database
+      $res = $conn->query("SELECT * FROM computers WHERE user_id='$_SESSION[user_id]'")->fetch_array();
+      if($res == 0){
+        $ip = $_SERVER["REMOTE_ADDR"];
+        $conn->query("INSERT INTO computers SET ip_address='$ip', user_id='$_SESSION[user_id]'");
       }else{
-        echo "
+        // Cek apakah IP saat ini sama dengan IP saat login sebelumnya
+        if($_SESSION['ip_address'] != $res['ip_address']){
+          echo "Maaf, Anda tidak diizinkan login dari IP yang berbeda.";
+          exit;
+        }else{
+          echo "
           <script>
-          alert('gagal');
-          </script>
-        ";
+            window.location.href='dashboard';
+            </script>
+          ";
+        }
       }
-      $res = $conn->query("select * from computers where user_id='$_SESSION[user_id]'");
-      while($result = $res->fetch_array()){
-        $allowed_ips = array($result['ip_address']);
-      }
-      $current_ip = $_SERVER['REMOTE_ADDR'];
-      print_r($allowed_ips);
+
+      // Set IP address dari user yang baru saja login
+      $_SESSION["ip_address"] = $_SERVER["REMOTE_ADDR"];
+    } else {
+      echo "Maaf, username atau password salah.";
     }
   }
 ?>
-<!--  -->
+
+
